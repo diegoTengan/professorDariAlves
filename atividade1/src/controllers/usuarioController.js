ab43e9c136a1068701bafb94b35b9246b01f9e4e
Diego Tengan <policarpotengan@outlook.com>
2022-06-25 09:09:49 -0300

    Versão 1.1

--- atividade1/src/controllers/usuarioController.js
+++ atividade1/src/controllers/usuarioController.js
@@ -1,10 +1,12 @@
+const Api = require('../models/Api');
 const home = (request, response)=> {
     return response.render('index');
 };
 
 
 const usuario = (request, response)=> {
-    return response.render('usuarios');
+    //const nomes = ['Bryan', 'Jordan', 'Kevin'];
+    return response.render('usuarios', {Api});
 };
 
 module.exports = {
---
+++ atividade1/src/models/Api.js
@@ -0,0 +1,24 @@
+const Api = [
+    {
+        id: 1,
+        name: "Diego",
+        email: "diego@gmail.com",
+    },
+    {
+        id: 2,
+        name: "Dari",
+        email: "dari@gmail.com",
+    }
+];
+
+module.exports = Api;
+
+
+
+
+
+
+
+//const Nomes = ["Bryan", "Jordan", "Kevin"];
+
+//module.exports = Nomes;
